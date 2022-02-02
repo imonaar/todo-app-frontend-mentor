820d7546b3f7ab04d6b06ba2584b308d726f6695
imonaar <kevinmonari22@gmail.com>
2022-02-02 11:35:38 +0300

    theme switch

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,33 @@
+import { ReactComponent as Moon } from "./icon-moon.svg";
+import { ReactComponent as Sun } from "./icon-sun.svg";
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const getCurrentTheme = () =>
+    window.matchMedia("(prefers-color-scheme: light)").matches;
+
+  const systemTheme = getCurrentTheme() ? "light" : "dark";
+
+  const [theme, setTheme] = useState(systemTheme);
+
+  useEffect(() => {
+    document.body.className = theme;
+    return () => (document.body.className = "");
+  }, [theme]);
+
+  const changeTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <nav>
+    <nav className="header">
       <section>
-        <h1>Todo App</h1>
+        <h1>Todo</h1>
       </section>
+      <button onClick={changeTheme}>
+        {theme === "light" ? <Moon /> : <Sun />}
+      </button>
     </nav>
   );
 }
